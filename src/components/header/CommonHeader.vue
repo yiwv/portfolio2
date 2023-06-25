983c6f9e3afb081a6e5e9ec7a90a2a924011ec5f
yiwv <y.ishii.wv@gmail.com>
2023-06-26 07:54:47 +0900

    refactor: CommonHeader内のjQueryを使った処理の置き換え

--- src/components/header/CommonHeader.vue
+++ src/components/header/CommonHeader.vue
@@ -1,30 +1,60 @@
 <script setup>
-import { onMounted } from 'vue';
+import { ref, onMounted, onUnmounted } from 'vue';
+import Sidebar from 'primevue/sidebar';
 
-onMounted(async () => {
-  $(".drawer").drawer();
-  await import("../../assets/js/common.js")
-});
+const sidebarIsVisible = ref(false);
 
 const spMenuBtnClick = (evt) => {
-  evt.preventDefault()
-  // drawer-toggleクラスを持つ要素が複数存在し、
-  // メニューの開閉をする関数がボタン以外にセットされている場合があるので、
-  // jQueryのclick関数で全てのdrawer-toggleクラス要素に対してクリックイベントを発生させる。
-  $('.drawer-toggle').click()
+  sidebarIsVisible.value = !sidebarIsVisible.value;
 };
+
+
+const headerNavRef = ref();
+
+// header 固定
+const handleScroll = () => {
+  const scrollTop = document.documentElement.scrollTop;
+  let scrollTopIsMoved = false;
+  let paddingValue = '';
+
+  if (window.matchMedia("(min-width:767px)").matches) {
+    scrollTopIsMoved = scrollTop > 95 - 70;
+    paddingValue = '95px';
+  } else {
+    scrollTopIsMoved = scrollTop > 65 - 60;
+    paddingValue = '65px';
+  }
+
+  if (scrollTopIsMoved) {
+    headerNavRef.value.classList.add('fixed');
+    document.body.style.paddingTop = paddingValue;
+  } else {
+    headerNavRef.value.classList.remove('fixed');
+    document.body.style.paddingTop = '0';
+  }
+}
+
+onMounted(() => {
+  // header固定のための処理
+  window.addEventListener('scroll', handleScroll)
+});
+
+onUnmounted(() => {
+  // コンポーネントの削除時にheader固定のためのイベントリスナーも削除する
+  window.removeEventListener('scroll', handleScroll)
+});
 
 </script>
 
 <template>
 <!-- header -->
-<header class="header">
+<header class="header" ref="headerNavRef">
   <h1 class="logo"><a href="/">ポケットウェブ</a></h1>
   <nav class="global-navi">
     <ul>
-      <li><a href="#news">NEWS</a></li>
-      <li><a href="#about">ABOUT</a></li>
-      <li><a href="#company">COMPANY</a></li>
+      <li><a v-smooth-scroll href="#news">NEWS</a></li>
+      <li><a v-smooth-scroll href="#about">ABOUT</a></li>
+      <li><a v-smooth-scroll href="#company">COMPANY</a></li>
       <li><a href="/chat/">CHAT SUPPORT</a></li>
     </ul>
   </nav>
@@ -35,15 +65,18 @@ const spMenuBtnClick = (evt) => {
     <span class="drawer-hamburger-icon"></span>
   </button>
 
-  <nav class="drawer-nav">
-    <ul class="drawer-menu">
-      <li><a href="/">TOP</a></li>
-      <li><a href="#news">NEWS</a></li>
-      <li><a href="#about">ABOUT</a></li>
-      <li><a href="#company">COMPANY</a></li>
-      <li><a href="/chat/">CHAT SUPPORT</a></li>
-    </ul>
-  </nav>
+  <Sidebar v-model:visible="sidebarIsVisible" position="right">
+    <nav class="drawer-nav_">
+      <ul class="drawer-menu">
+        <li><a href="/">TOP</a></li>
+        <li><a v-smooth-scroll href="#news">NEWS</a></li>
+        <li><a v-smooth-scroll href="#about">ABOUT</a></li>
+        <li><a v-smooth-scroll href="#company">COMPANY</a></li>
+        <li><a href="/chat/">CHAT SUPPORT</a></li>
+      </ul>
+    </nav>
+  </Sidebar>
+
   <!--sp menu end-->
 </header>
 </template>
